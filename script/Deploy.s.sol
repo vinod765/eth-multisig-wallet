81f10fd0729484bfed0c7363a4436e3b7ca47f2b
vinod765 <vinodekdhoke@gmail.com>
2026-03-16 07:39:35 +0530

    feat: add deployment script for MultiSigWallet

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,2 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+import {Script, console} from "forge-std/Script.sol";
+import {MultiSigWallet} from "../src/MultiSigWallet.sol";
+
+contract DeployMultiSigWallet is Script {
+    function run() external returns (MultiSigWallet) {
+        uint deployerKey = vm.envUint("PRIVATE_KEY");
+
+        address[] memory owners = new address[](3);
+        owners[0] = vm.envAddress("OWNER1");
+        owners[1] = vm.envAddress("OWNER2");
+        owners[2] = vm.envAddress("OWNER3");
+
+        uint required = 2;
+
+        vm.startBroadcast(deployerKey);
+        MultiSigWallet wallet = new MultiSigWallet(owners, required);
+        vm.stopBroadcast();
+
+        console.log("MultiSigWallet deployed at:", address(wallet));
+        return wallet;
+    }
+}
